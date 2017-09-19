70eb290aa1f717b0e242b9a29b1da19d448701d3
xushenkun <xushenkun@gmail.com>
2017-09-19 12:37:16 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,3 @@
+
+cd /application/search/tf/gnmt/nmt_git_12
+nohup python3 -m nmt.nmt --src=zh --tgt=en --vocab_prefix=../corpus/en_zh/vocab --train_prefix=../corpus/en_zh/train --dev_prefix=../corpus/en_zh/valid --test_prefix=../corpus/en_zh/test --out_dir=../models --num_train_steps=12000 --steps_per_stats=100 --num_gpus=4 --num_layers=4 --num_units=128 --dropout=0.2 --metrics=bleu --attention=scaled_luong --attention_architecture=gnmt_v2 &
