5536588c7f3fd512959455562284dc107ea18a8f
xushenkun <xushenkun@gmail.com>
2017-09-20 17:33:10 +0800

    add script for zh en

--- cmds/preprocess.sh
+++ cmds/preprocess.sh
@@ -4,7 +4,7 @@ current_dir=`dirname $0`
 
 . $current_dir/env_vars
 
-#corpus train
+echo "prepare corpus train..."
 cd $corpus_home
 pwd
 unzip datum2017.zip
@@ -12,21 +12,21 @@ cd datum2017
 cat Book1_cn.txt Book2_cn.txt Book3_cn.txt Book4_cn.txt Book5_cn.txt Book6_cn.txt Book7_cn.txt Book8_cn.txt Book9_cn.txt Book10_cn.txt > $corpus_home/train.zh
 cat Book1_en.txt Book2_en.txt Book3_en.txt Book4_en.txt Book5_en.txt Book6_en.txt Book7_en.txt Book8_en.txt Book9_en.txt Book10_en.txt > $corpus_home/train.en
 
-#corpus dev
+echo "prepare corpus dev..."
 for year in 2017;
 do
   $moses_scripts/ems/support/input-from-sgm.perl < $corpus_home/dev/newsdev${year}-zhen-ref.en.sgm > $corpus_home/newsdev$year.en
   $moses_scripts/ems/support/input-from-sgm.perl < $corpus_home/dev/newsdev${year}-zhen-src.zh.sgm > $corpus_home/newsdev$year.zh
 done
 
-#corpus test
+echo "prepare corpus test..."
 for year in 2017;
 do
   $moses_scripts/ems/support/input-from-sgm.perl < $corpus_home/test/newstest${year}-zhen-ref.en.sgm > $corpus_home/newstest$year.en
   $moses_scripts/ems/support/input-from-sgm.perl < $corpus_home/test/newstest${year}-zhen-src.zh.sgm > $corpus_home/newstest$year.zh
 done
 
-#tokenize
+echo "tokenize corpus..."
 for prefix in train newsdev2017 newstest2017
  do
    export PYTHONPATH=$jieba_home
@@ -38,7 +38,7 @@ for prefix in train newsdev2017 newstest2017
    $moses_scripts/tokenizer/tokenizer.perl -a -threads 8 -l en > $corpus_home/$prefix.tok.en
  done
 
-#clean
+echo "clean and case corpus..."
 $moses_scripts/training/clean-corpus-n.perl $corpus_home/train.tok zh en $corpus_home/train.tok.clean 1 100
 $moses_scripts/recaser/train-truecaser.perl -corpus $corpus_home/train.tok.clean.zh -model $modles_home/truecase-model.zh
 $moses_scripts/recaser/train-truecaser.perl -corpus $corpus_home/train.tok.clean.en -model $modles_home/truecase-model.en
@@ -50,7 +50,7 @@ for prefix in newsdev2017 newstest2017
   $moses_scripts/recaser/truecase.perl -model $modles_home/truecase-model.en < $corpus_home/$prefix.tok.en > $corpus_home/$prefix.tc.en
  done
 
-#bpe
+echo "make bpe for corpus..."
 $bpe_home/learn_joint_bpe_and_vocab.py -i $corpus_home/train.tc.zh $corpus_home/train.tc.en --write-vocabulary $corpus_home/vocab.zh $corpus_home/vocab.en -s $bpe_operations -o $modles_home/zhen.bpe
 for prefix in train newsdev2017 newstest2017
  do
@@ -59,8 +59,11 @@ for prefix in train newsdev2017 newstest2017
   $bpe_home/apply_bpe.py -c $modles_home/zhen.bpe --vocabulary $corpus_home/vocab.en --vocabulary-threshold $bpe_threshold < $corpus_home/$prefix.tc.en > $corpus_home/$prefix.bpe.en
  done
 
-#dictionary
+echo "make dictionary for corpus..."
 echo -e "<unk>\n<s>\n</s>" > "${corpus_home}/vocab.bpe"
 cat "${corpus_home}/train.bpe.zh" "${corpus_home}/train.bpe.en" | ${bpe_home}/get_vocab.py | cut -f1 -d ' ' >> "${corpus_home}/vocab.bpe"
 cp "${corpus_home}/vocab.bpe" "${corpus_home}/vocab.bpe.zh"
 cp "${corpus_home}/vocab.bpe" "${corpus_home}/vocab.bpe.en"
+
+echo "preprocess over!"
+
